eac3882f8e5a42b2ef5d94e2b920b7b320945b17
Thibault Bodeux <thibault.bodeux@gmx.com>
2011-11-27 23:35:07 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Archintranet::Application.config.secret_token = 'c76757e842de1981829a33be4921c1235e6d373be584299d93cd7aa92c9c04c706bbf0415b2bb0f7d4289f0739bd82e9fe928aee9f576c4675c18ea849f8a14e'
